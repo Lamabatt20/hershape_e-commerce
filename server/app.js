0af6745a841a4d01071e4c338d5e1c894837ee18
lama batta <154995832+Lamabatt20@users.noreply.github.com>
2025-10-11 21:13:52 +0300

    Update

--- server/app.js
+++ server/app.js
@@ -47,10 +47,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
   },
-  tls: {
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 10000, 
 });
 
 // ====== AUTH ======
